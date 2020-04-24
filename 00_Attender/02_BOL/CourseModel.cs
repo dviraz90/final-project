59928b6e9217edcf3bd364b2402260af438dab44
dvir <dviraz90@gmail.com>
2020-04-24 13:24:22 +0300

    dvir

---
+++ 00_Attender/02_BOL/CourseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_BOL
+{
+   public class CourseModel
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int LectureId { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
